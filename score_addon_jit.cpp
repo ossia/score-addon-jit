0049206b6751ad70b9eb0bc4f176d70c95abe24e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-03-26 12:26:37 +0200

    first setup

---
+++ score_addon_jit.cpp
@@ -0,0 +1,443 @@
+#include "score_addon_jit.hpp"
+#include <score/plugins/customfactory/FactorySetup.hpp>
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//
+// Based on the code in
+// https://github.com/weliveindetail/JitFromScratch
+//
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Hack: cc1 lives in "tools" next to "include"
+#include <../tools/driver/cc1_main.cpp>
+
+#include <llvm/ADT/SmallString.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/JITEventListener.h>
+#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
+#include <llvm/ExecutionEngine/Orc/OrcError.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/RuntimeDyld.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/Mangler.h>
+#include <llvm/Support/Debug.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/Error.h>
+
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/ManagedStatic.h>
+#include <llvm/Support/PrettyStackTrace.h>
+#include <llvm/Support/Signals.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <ossia/dataflow/execution_state.hpp>
+#include <ossia/dataflow/graph_node.hpp>
+
+
+#include <functional>
+#include <memory>
+#include <vector>
+#include <string>
+#define DEBUG_TYPE "score_addon_jit"
+#define STRINGIFY_DETAIL(X) #X
+#define STRINGIFY(X) STRINGIFY_DETAIL(X)
+
+std::vector<std::string> getClangCC1Args(llvm::StringRef cpp,
+                                         llvm::StringRef bc) {
+  std::vector<std::string> args;
+
+  args.push_back("-emit-llvm");
+  args.push_back("-emit-llvm-bc");
+  args.push_back("-emit-llvm-uselists");
+
+  args.push_back("-main-file-name");
+  args.push_back(cpp.data());
+
+  args.push_back("-std=c++1z");
+  args.push_back("-disable-free");
+  args.push_back("-fdeprecated-macro");
+  args.push_back("-fmath-errno");
+  args.push_back("-fuse-init-array");
+
+  args.push_back("-mrelocation-model");
+  args.push_back("static");
+  args.push_back("-mthread-model");
+  args.push_back("posix");
+  args.push_back("-masm-verbose");
+  args.push_back("-mconstructor-aliases");
+  args.push_back("-munwind-tables");
+
+  args.push_back("-dwarf-column-info");
+  args.push_back("-debugger-tuning=gdb");
+
+  args.push_back("-fcxx-exceptions");
+  args.push_back("-fno-use-cxa-atexit");
+
+  args.push_back("-O3");
+  // does not work : args.push_back("-march=core-avx2");
+  args.push_back("-mdisable-fp-elim");
+  args.push_back("-momit-leaf-frame-pointer");
+  args.push_back("-vectorize-loops");
+  args.push_back("-vectorize-slp");
+
+  args.push_back("-resource-dir");
+  args.push_back(STRINGIFY(JIT_FROM_SCRATCH_CLANG_RESOURCE_DIR));
+
+  args.push_back("-internal-isystem");
+  args.push_back(STRINGIFY(JIT_FROM_SCRATCH_CLANG_RESOURCE_DIR) "/include");
+  args.push_back("-I");
+  args.push_back("/usr/include/c++/7.3.1/x86_64-pc-linux-gnu");
+  args.push_back("-I");
+  args.push_back("/usr/include/c++/7.3.1");
+  args.push_back("-I");
+  args.push_back("/usr/include/x86_64-linux-gnu");
+  args.push_back("-I");
+  args.push_back("/usr/include");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/OSSIA");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/3rdparty/nano-signal-slot/include");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/3rdparty/brigand/include");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/3rdparty/variant/include");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/3rdparty/SmallFunction/smallfun/include");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/i-score/API/3rdparty/ModernMIDI");
+  args.push_back("-I");
+  args.push_back("/home/jcelerier/travail/build-i-score-Sanitized-Debug/API/OSSIA");
+  args.push_back("-I");
+  args.push_back("./include");
+
+  args.push_back("-o");
+  args.push_back(bc.data());
+  args.push_back("-x");
+  args.push_back("c++");
+  args.push_back(cpp.data());
+
+  return args;
+}
+
+#undef STRINGIFY
+#undef STRINGIFY_DETAIL
+
+class ClangCC1Driver {
+public:
+  ClangCC1Driver() = default;
+
+  // As long as the driver exists, source files remain on disk to allow
+  // debugging JITed code.
+  ~ClangCC1Driver() {
+    for (auto D : SoucreFileDeleters)
+      D();
+  }
+
+  llvm::Expected<std::unique_ptr<llvm::Module>>
+  compileTranslationUnit(std::string cppCode, llvm::LLVMContext &context);
+
+private:
+  std::vector<std::function<void()>> SoucreFileDeleters;
+};
+
+namespace {
+
+llvm::Error return_code_error(llvm::StringRef message, int returnCode) {
+  return llvm::make_error<llvm::StringError>(
+      message, std::error_code(returnCode, std::system_category()));
+}
+
+llvm::Expected<std::string> saveSourceFile(std::string content) {
+  using llvm::sys::fs::createTemporaryFile;
+
+  int fd;
+  llvm::SmallString<128> name;
+  if (auto ec = createTemporaryFile("JitFromScratch", "cpp", fd, name))
+    return llvm::errorCodeToError(ec);
+
+  constexpr bool shouldClose = true;
+  constexpr bool unbuffered = true;
+  llvm::raw_fd_ostream os(fd, shouldClose, unbuffered);
+  os << content;
+
+  return name.str();
+}
+
+std::string replaceExtension(llvm::StringRef name, llvm::StringRef ext) {
+  return name.substr(0, name.find_last_of('.') + 1).str() + ext.str();
+}
+
+llvm::Error compileCppToBitcodeFile(std::vector<std::string> args) {
+  DEBUG({
+    llvm::dbgs() << "Invoke Clang cc1 with args:\n";
+    for (std::string arg : args)
+      llvm::dbgs() << arg << " ";
+    llvm::dbgs() << "\n\n";
+  });
+
+  std::vector<const char *> argsX;
+  std::transform(args.begin(), args.end(), std::back_inserter(argsX),
+                 [](const std::string &s) { return s.c_str(); });
+
+  if (int res = cc1_main(argsX, "", nullptr))
+    return return_code_error("Clang cc1 compilation failed", res);
+
+  return llvm::Error::success();
+}
+
+llvm::Expected<std::unique_ptr<llvm::Module>>
+readModuleFromBitcodeFile(llvm::StringRef bc, llvm::LLVMContext &context) {
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> buffer =
+      llvm::MemoryBuffer::getFile(bc);
+  if (!buffer)
+    return llvm::errorCodeToError(buffer.getError());
+
+  return llvm::parseBitcodeFile(buffer.get()->getMemBufferRef(), context);
+}
+
+} // end namespace
+
+llvm::Expected<std::unique_ptr<llvm::Module>>
+ClangCC1Driver::compileTranslationUnit(std::string cppCode,
+                                       llvm::LLVMContext &context) {
+  auto sourceFileName = saveSourceFile(cppCode);
+  if (!sourceFileName)
+    return sourceFileName.takeError();
+
+  std::string cpp = *sourceFileName;
+  std::string bc = replaceExtension(cpp, "bc");
+
+  llvm::Error err = compileCppToBitcodeFile(getClangCC1Args(cpp, bc));
+  if (err)
+    return std::move(err);
+
+  auto module = readModuleFromBitcodeFile(bc, context);
+
+  llvm::sys::fs::remove(bc);
+
+  if (!module) {
+    llvm::sys::fs::remove(cpp);
+    return module.takeError();
+  }
+
+  SoucreFileDeleters.push_back([cpp]() { llvm::sys::fs::remove(cpp); });
+
+  return std::move(*module);
+}
+
+class SimpleOrcJit {
+  struct NotifyObjectLoaded_t {
+    NotifyObjectLoaded_t(SimpleOrcJit &jit) : Jit(jit) {}
+
+    // Called by the ObjectLayer for each emitted object.
+    // Forward notification to GDB JIT interface.
+    void
+    operator()(llvm::orc::RTDyldObjectLinkingLayerBase::ObjHandleT,
+               const llvm::orc::RTDyldObjectLinkingLayerBase::ObjectPtr &obj,
+               const llvm::LoadedObjectInfo &info) {
+
+      // Workaround 5.0 API inconsistency:
+      // http://lists.llvm.org/pipermail/llvm-dev/2017-August/116806.html
+      const auto &fixedInfo =
+          static_cast<const llvm::RuntimeDyld::LoadedObjectInfo &>(info);
+
+      Jit.GdbEventListener->NotifyObjectEmitted(*obj->getBinary(), fixedInfo);
+    }
+
+  private:
+    SimpleOrcJit &Jit;
+  };
+
+  using ModulePtr_t = std::unique_ptr<llvm::Module>;
+  using IRCompiler_t = llvm::orc::SimpleCompiler;
+
+  using ObjectLayer_t = llvm::orc::RTDyldObjectLinkingLayer;
+  using CompileLayer_t = llvm::orc::IRCompileLayer<ObjectLayer_t, IRCompiler_t>;
+
+public:
+  SimpleOrcJit(llvm::TargetMachine &targetMachine)
+      : DL(targetMachine.createDataLayout()),
+        MemoryManagerPtr(std::make_shared<llvm::SectionMemoryManager>()),
+        SymbolResolverPtr(llvm::orc::createLambdaResolver(
+            [&](std::string name) { return findSymbolInJITedCode(name); },
+            [&](std::string name) { return findSymbolInHostProcess(name); })),
+        NotifyObjectLoaded(*this),
+        ObjectLayer([this]() { return MemoryManagerPtr; }, NotifyObjectLoaded),
+        CompileLayer(ObjectLayer, IRCompiler_t(targetMachine)) {
+    // Load own executable as dynamic library.
+    // Required for RTDyldMemoryManager::getSymbolAddressInProcess().
+    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+
+    // Internally points to a llvm::ManagedStatic.
+    // No need to free. "create" is a misleading term here.
+    GdbEventListener = llvm::JITEventListener::createGDBRegistrationListener();
+  }
+
+  void submitModule(ModulePtr_t module) {
+    DEBUG({
+      llvm::dbgs() << "Submit LLVM module:\n\n";
+      llvm::dbgs() << *module.get() << "\n\n";
+    });
+
+    // Commit module for compilation to machine code. Actual compilation
+    // happens on demand as soon as one of it's symbols is accessed. None of
+    // the layers used here issue Errors from this call.
+    llvm::cantFail(
+        CompileLayer.addModule(std::move(module), SymbolResolverPtr));
+  }
+
+  llvm::Expected<std::unique_ptr<llvm::Module>>
+  compileModuleFromCpp(std::string cppCode, llvm::LLVMContext &context) {
+    return ClangDriver.compileTranslationUnit(cppCode, context);
+  }
+
+  template <class Signature_t>
+  llvm::Expected<std::function<Signature_t>> getFunction(std::string name) {
+    using namespace llvm;
+
+    // Find symbol name in committed modules.
+    std::string mangledName = mangle(std::move(name));
+    JITSymbol sym = findSymbolInJITedCode(mangledName);
+    if (!sym)
+      return make_error<orc::JITSymbolNotFound>(mangledName);
+
+    // Access symbol address.
+    // Invokes compilation for the respective module if not compiled yet.
+    Expected<JITTargetAddress> addr = sym.getAddress();
+    if (!addr)
+      return addr.takeError();
+
+    auto typedFunctionPtr = reinterpret_cast<Signature_t *>(*addr);
+    return std::function<Signature_t>(typedFunctionPtr);
+  }
+
+private:
+  llvm::DataLayout DL;
+  ClangCC1Driver ClangDriver;
+  std::shared_ptr<llvm::RTDyldMemoryManager> MemoryManagerPtr;
+  std::shared_ptr<llvm::JITSymbolResolver> SymbolResolverPtr;
+  NotifyObjectLoaded_t NotifyObjectLoaded;
+  llvm::JITEventListener *GdbEventListener;
+
+  ObjectLayer_t ObjectLayer;
+  CompileLayer_t CompileLayer;
+
+  llvm::JITSymbol findSymbolInJITedCode(std::string mangledName) {
+    constexpr bool exportedSymbolsOnly = false;
+    return CompileLayer.findSymbol(mangledName, exportedSymbolsOnly);
+  }
+
+  llvm::JITSymbol findSymbolInHostProcess(std::string mangledName) {
+    // Lookup function address in the host symbol table.
+    if (llvm::JITTargetAddress addr =
+            llvm::RTDyldMemoryManager::getSymbolAddressInProcess(mangledName))
+      return llvm::JITSymbol(addr, llvm::JITSymbolFlags::Exported);
+
+    return nullptr;
+  }
+
+  // System name mangler: may prepend '_' on OSX or '\x1' on Windows
+  std::string mangle(std::string name) {
+    std::string buffer;
+    llvm::raw_string_ostream ostream(buffer);
+    llvm::Mangler::getNameWithPrefix(ostream, std::move(name), DL);
+    return ostream.str();
+  }
+};
+
+// Show the error message and exit.
+LLVM_ATTRIBUTE_NORETURN static void fatalError(llvm::Error E) {
+  llvm::handleAllErrors(std::move(E), [&](const llvm::ErrorInfoBase &EI) {
+    llvm::errs() << "Fatal Error: ";
+    EI.log(llvm::errs());
+    llvm::errs() << "\n";
+    llvm::errs().flush();
+  });
+
+  exit(1);
+}
+
+score_addon_jit::score_addon_jit()
+{
+  using namespace llvm;
+
+  int argc = 0;
+  char** argv = nullptr;
+  sys::PrintStackTraceOnErrorSignal("/usr/bin/false");
+  PrettyStackTraceProgram X(argc, argv);
+
+  atexit(llvm_shutdown);
+
+  InitializeNativeTarget();
+  InitializeNativeTargetAsmPrinter();
+  InitializeNativeTargetAsmParser();
+
+  // Parse -debug and -debug-only options.
+  cl::ParseCommandLineOptions(argc, argv, "JitFromScratch example project\n");
+
+  auto targetMachine = EngineBuilder().selectTarget();
+  auto jit = std::make_unique<SimpleOrcJit>(*targetMachine);
+
+  // Implementation of the integerDistances function.
+  std::string sourceCode =
+      "#include <vector>                                    \n"
+      "#include <ossia/dataflow/graph_node.hpp>          \n"
+      "struct foo : ossia::graph_node { "
+      " void run(ossia::token_request t, ossia::execution_state&) "
+      " { std::cerr << \" oh wow \" << t.date.impl << std::endl; }"
+      "};"
+
+      "extern \"C\" ossia::graph_node* vec() {          \n"
+      "  return new foo;                                          \n"
+      "}                                                    \n";
+
+  // Compile C++ to bitcode.
+  LLVMContext context;
+  auto module = jit->compileModuleFromCpp(sourceCode, context);
+  if (!module)
+    fatalError(module.takeError());
+
+  // Compile to machine code and link.
+  jit->submitModule(std::move(*module));
+  auto jitedFn =
+      jit->getFunction<ossia::graph_node* ()>("vec");
+  if (!jitedFn)
+    fatalError(jitedFn.takeError());
+
+  // Invoke JITed function.
+  auto integerDistances = *jitedFn;
+  auto v = integerDistances();
+  ossia::execution_state e;
+  v->run({}, e);
+
+  outs().flush();
+}
+
+score_addon_jit::~score_addon_jit()
+{
+
+}
+
+std::vector<std::unique_ptr<score::InterfaceBase> >
+score_addon_jit::factories(
+        const score::ApplicationContext& ctx,
+        const score::InterfaceKey& key) const
+{
+    return instantiate_factories<  >(ctx, key);
+}
---
+++ score_addon_jit.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <score/plugins/qt_interfaces/PluginRequirements_QtInterface.hpp>
+#include <score/plugins/qt_interfaces/FactoryInterface_QtInterface.hpp>
+#include <score/plugins/qt_interfaces/CommandFactory_QtInterface.hpp>
+
+#include <score/application/ApplicationContext.hpp>
+#include <score/plugins/customfactory/FactoryFamily.hpp>
+
+#include <QObject>
+#include <utility>
+#include <vector>
+
+class score_addon_jit final :
+        public QObject,
+        public score::Plugin_QtInterface,
+        public score::FactoryInterface_QtInterface
+{
+        Q_OBJECT
+        Q_PLUGIN_METADATA(IID FactoryInterface_QtInterface_iid)
+        Q_INTERFACES(
+                score::Plugin_QtInterface
+                score::FactoryInterface_QtInterface
+                )
+
+  SCORE_PLUGIN_METADATA(1, "3be955fe-4a09-498b-834a-25df3b7a8ca5")
+
+    public:
+        score_addon_jit();
+        virtual ~score_addon_jit();
+
+    private:
+        // Defined in FactoryInterface_QtInterface
+        std::vector<std::unique_ptr<score::InterfaceBase>> factories(
+                const score::ApplicationContext& ctx,
+                const score::InterfaceKey& key) const override;
+
+};
